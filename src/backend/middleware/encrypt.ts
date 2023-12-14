875794a5d84167c6388780f3a88300119e50a739
avila-ca <118176131+avila-ca@users.noreply.github.com>
2023-12-14 13:00:00 +0100

    working at register

--- src/backend/middleware/encrypt.ts
+++ src/backend/middleware/encrypt.ts
@@ -1,10 +1,10 @@
 import bcrypt from 'bcrypt';
 
-export const encrypt = async (plainPassword:string) => {
-    const hash = await bcrypt.hash(plainPassword, 10);
-    return hash;
-}
+export const encrypt = async (plainPassword: string) => {
+  const hash = await bcrypt.hash(plainPassword, 10);
+  return hash;
+};
 
-export const compare = (plainPassword:string, hashPassword:string) => {
-    return bcrypt.compare(plainPassword, hashPassword)
-}
+export const compare = async (plainPassword: string, hashPassword: string) => {
+  return await bcrypt.compare(plainPassword, hashPassword);
+};
--- src/dices/infrastructure/mongoDB/mongoRepository/MongoRepository.ts
+++ src/dices/infrastructure/mongoDB/mongoRepository/MongoRepository.ts
@@ -5,46 +5,75 @@ import { UserModel } from '../mongoModel/UserSchema';
 
 export class MongoGameRepository implements GameRepository {
   async postNewUser(newUser: Player): Promise<boolean> {
-    const hashPassword = await encrypt(newUser.password)
+    const hashPassword = await encrypt(newUser.password);
 
-      const isRegistered = await UserModel.find({
-        name:newUser.name,
-      })
-      if (isRegistered) {
-        const comparePasswords = async () => { 
-          isRegistered.map(async (value) => {
-          const isSamePass = await compare(newUser.password, value.password)
-          if (isSamePass)
-            return true;
-        })
+    const isRegistered = await UserModel.find({
+      name: newUser.name
+    });
+    const comparePasswords = isRegistered.map(
+      async (value: { password: string }) => {
+        const isSamePass = () => {
+          return compare(newUser.password, value.password);
+        };
+        const isPassRegistered = await isSamePass();
+        if (isPassRegistered) {
+          console.log('Login please!! There is user with your credentials');
+          return null;
+        } else {
+          return value;
+        }
       }
-      const comparation = await comparePasswords()
-      
-        console.log(' ahi va '+ comparation)
-      
-      console.log('false  ');
+    );
+    
+    if (comparePasswords.length) {
+      console.log('false  ' + comparePasswords);
       return false;
-      }
+    } else {
+      const newUserRegistered: Player = {
+        name: newUser.name,
+        password: hashPassword
+      };
       console.log('true');
-      await UserModel.create(newUser);
+      await UserModel.create(newUserRegistered);
       return true;
+    }
   }
 
   async postUserLogin(newUser: Player): Promise<Player | null> {
-    
-    const isNameRegistered = await UserModel.findOne({
-      name: newUser.name 
+    const hashPassword = await encrypt(newUser.password);
+
+    const isNameRegistered = await UserModel.find({
+      name: newUser.name
     });
-  
-    const isPassRegistered = await UserModel.findOne({
-      password: newUser.password
-    });
-    if (isPassRegistered && isNameRegistered) {
-      console.log('success', newUser.name);
+    const comparePasswords = isNameRegistered.map(
+      async (value: { password: string }) => {
+        const isSamePass = async (): Promise<boolean> => {
+          return compare(newUser.password, value.password);
+        };
+        const isPassRegistered = await isSamePass();
+        if (isPassRegistered) {
+          console.log('Wellcome ');
+          return value;
+        }
+      }
+    );
+    if (comparePasswords.length == 1) {
+      console.log('Yesss ' + comparePasswords);
       return newUser;
     } else {
-      console.log('try  again', newUser.name);
+      console.log('false');
       return null;
     }
+
+    // const isPassRegistered = await UserModel.findOne({
+    //   password: newUser.password
+    // });
+    // if (isPassRegistered && isNameRegistered) {
+    //   console.log('success', newUser.name);
+    //   return newUser;
+    // } else {
+    //   console.log('try  again', newUser.name);
+    //   return null;
+    // }
   }
 }
