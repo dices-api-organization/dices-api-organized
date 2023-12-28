df0915a87a8601d73f1101b23905e7cfe3acd159
avila-ca <avilacalaf@gmail.com>
2023-12-28 17:17:21 +0100

    delete selectBD react component

--- src/dices/infrastructure/front/src/App.tsx
+++ src/dices/infrastructure/front/src/App.tsx
@@ -3,7 +3,6 @@ import { Header } from './components/Header'
 import { LoginForm } from './components/LoginForm'
 import {Routes, Route} from 'react-router-dom'
 import { RegisterForm } from './components/RegisterForm'
-import { SelectBD } from './components/SelectBD'
 import { Play } from './components/Play'
 import { useState } from 'react'
 import { UpdateForm } from './components/UpdateForm'
@@ -11,15 +10,12 @@ import { UpdateForm } from './components/UpdateForm'
 
 function App(): JSX.Element {
   const [userId, setUserId] = useState('')
-  //const [userName, setUserName] = useState('')
-  //const setUserData = (userId: string, )
 
   return (
     <>
         <Header />
        <Routes>
-        <Route path='/' element={<SelectBD />} />
-        <Route path='/login' element={<LoginForm setUserId={setUserId}/>} />
+        <Route path='/' element={<LoginForm setUserId={setUserId}/>} />
         <Route path='/register' element={<RegisterForm setUserId={setUserId}/>} />
         <Route path='/play'  element={<Play id={userId} />} />
         <Route path='/play/update'  element={<UpdateForm userId={userId} />} />
